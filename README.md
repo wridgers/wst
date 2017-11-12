7fe1e979ff7e2c16646d7a65326a3ec97f2fc72e
Will Ridgers <will@mindfuzz.net>
2017-11-12 10:09:55 +0000

    v0.1.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# WebSocket Tool
+
+A utility for working with websockets.
+
+## Install
+
+    cargo install wst
+
+## Usage
+
+`wst` can act as both a client and a server.
+
+    $ wst
+    Usage: wst MODE
+
+    Modes:
+      server HOST PORT  - a server that broadcasts stdin to connected clients
+      client URL        - a client that prints messages to stdout (default)
+
+As a **server** `wst` will transmit lines received on stdin to connected clients.
+
+    ( while :; do echo `date` && sleep 1; done) | wst server localhost 1234
+
+As a **client** `wst` will print received messages as a line to stdout. The `client` mode argument is optional.
+
+    wst client ws://localhost:1234
+    wst ws://host:port
+
+## Licence
+
+MIT
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+extern crate ws;
+
+use std::env;
+use std::io::BufRead;
+use std::io;
+use std::process::exit;
+use std::thread;
+
+use ws::{connect, Handler, Handshake, Message, Result, WebSocket};
+
+struct Client {}
+
+impl Handler for Client {
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        Ok(())
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("{}", msg);
+        Ok(())
+    }
+}
+
+fn usage_general() {
+    println!("Usage: wst MODE
+
+Modes:
+  server HOST PORT  - a server that broadcasts stdin to connected clients
+  client URL        - a client that prints messages to stdout (default)");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        usage_general();
+        exit(0);
+    }
+
+    let arg = &args[1];
+
+    match &arg[..] {
+        "help" => {
+            usage_general();
+        },
+
+        "server" => {
+            if args.len() < 4 {
+                usage_general();
+                exit(0);
+            }
+
+            let listen = format!("{}:{}", &args[2], &args[3]);
+            let server = WebSocket::new(|_| { move |_| { Ok(()) }}).unwrap();
+            let broadcaster = server.broadcaster();
+
+            let input = thread::spawn(move || {
+                let stdin = io::stdin();
+
+                for line in stdin.lock().lines() {
+                    // FIXME: don't copy here. use read_line instead.
+                    let r_line = line.unwrap();
+
+                    println!("{}", r_line);
+                    broadcaster.send(r_line).unwrap();
+                }
+            });
+
+            println!("Starting server on {}", listen);
+            server.listen(listen).unwrap();
+            input.join().unwrap()
+        },
+
+        "client" => {
+            if args.len() < 3 {
+                usage_general();
+                exit(0);
+            }
+
+            let url = &args[2];
+            connect(url.to_string(), |_| Client {}).unwrap()
+        },
+
+        _ => {
+            connect(arg.to_string(), |_| Client {}).unwrap()
+        }
+    }
+}
