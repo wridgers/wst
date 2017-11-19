02675d6d1109fac8dff2973b0c9379a3014f69ef
Will Ridgers <will@mindfuzz.net>
2017-11-19 23:31:24 +0000

    Add getopts and detailed help output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate getopts;
 extern crate ws;
 
 mod buffer;
@@ -11,6 +12,7 @@ use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
+use getopts::Options;
 use ws::{connect, WebSocket};
 
 use buffer::{Buffer};
@@ -24,10 +26,28 @@ fn usage_general() {
 Usage: wst MODE [arguments]
 
 Modes:
-  server HOST PORT  - a server that broadcasts stdin to connected clients
-  client URL        - a client that prints messages to stdout (default)
-  version           - show version
-  help              - show help", VERSION);
+  server HOST PORT      A server that broadcasts stdin to connected clients
+  client URL            A client that prints messages to stdout (default)
+  version               Show version
+  help                  Show help
+
+Run 'wst MODE help' for more information on a mode.", VERSION);
+}
+
+fn usage_server() {
+    println!("Usage: wst server [OPTIONS] HOST PORT
+
+Reads from STDIN and broadcasts each line to all clients as a messages
+
+Options:
+  -h N, --header N      The first N lines received on STDIN will be repeated to every new client
+  -b N, --banner N      The last N sent messages will be repeated to every new client, excluding header messages");
+}
+
+fn usage_client() {
+    println!("Usage: wst client URL
+
+Connects to a WebSocket and prints each message received to STDOUT");
 }
 
 fn main() {
@@ -50,14 +70,60 @@ fn main() {
         }
 
         "server" => {
-            if args.len() < 4 {
-                usage_general();
+            if args.len() < 4 || (args.len() == 3 && &args[2] == "help") {
+                usage_server();
                 exit(0);
             }
 
-            let buffer = Arc::new(Mutex::new(Buffer::new(1, 10)));
+            let listen_addr = &args[args.len() - 2];
+            let listen_port = &args[args.len() - 1];
 
-            let listen = format!("{}:{}", &args[2], &args[3]);
+            let mut server_opts = Options::new();
+            server_opts.optopt("b", "buffer", "length of buffer", "LENGTH");
+            server_opts.optopt("h", "header", "length of header", "LENGTH");
+
+            let matches = match server_opts.parse(&args[2..]) {
+                Ok(m) => { m },
+                Err(f) => {
+                    eprintln!("Error: {}", f);
+                    usage_server();
+                    exit(1);
+                },
+            };
+
+            let header_length = match matches.opt_str("h") {
+                Some(x) => {
+                    match x.parse::<usize>() {
+                        Ok(x) => { x },
+                        Err(f) => {
+                            eprintln!("Error: {}", f);
+                            usage_server();
+                            exit(1);
+                        },
+                    }
+                },
+                None => { 0 },
+            };
+
+            let buffer_length = match matches.opt_str("b") {
+                Some(x) => {
+                    match x.parse::<usize>() {
+                        Ok(x) => { x },
+                        Err(f) => {
+                            eprintln!("Error: {}", f);
+                            usage_server();
+                            exit(1);
+                        },
+                    }
+                },
+                None => { 0 },
+            };
+
+            eprintln!("Header length: {}", header_length);
+            eprintln!("Buffer length: {}", buffer_length);
+
+            let buffer = Arc::new(Mutex::new(Buffer::new(header_length, buffer_length)));
+
             let server = WebSocket::new(|conn| {
                 let buffer_clone = buffer.clone();
 
@@ -84,14 +150,15 @@ fn main() {
                 }
             });
 
+            let listen = format!("{}:{}", listen_addr, listen_port);
             println!("Starting server on {}", listen);
             server.listen(listen).unwrap();
             input.join().unwrap()
         },
 
         "client" => {
-            if args.len() < 3 {
-                usage_general();
+            if args.len() < 3 || &args[2] == "help" {
+                usage_client();
                 exit(0);
             }
 
